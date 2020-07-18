5db624c5e4e1e8bd7ebf701db33d625070bd2bee
Joel Sequeira <joensg@gmail.com>
2020-07-18 16:22:42 +0100

    add cafe code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import BootstrapVue from 'bootstrap-vue'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
